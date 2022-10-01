f43e2ed5193bbe6600e21dd23c93d3273648e059
Seshan Ravikumar <seshan10@me.com>
2022-10-01 17:07:27 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -69,7 +69,7 @@ build_image () {
     sudo chroot ./mnt /bin/busybox sh  <<'EOF'
         echo "(chroot) Upgrading to Plasma Mobile Nightly packages..."
         /sbin/apk update && /sbin/apk upgrade
-        /sbin/apk add nano bash neofetch htop
+        /sbin/apk add nano bash neofetch htop flatpak
         /sbin/apk add alligator angelfish audiotube calindori kalk kasts kclock keysmith koko krecorder ktrip kweather neochat plasma-dialer plasma-phonebook plasma-settings plasmatube qmlkonsole spacebar tokodon okular elisa plasma-camera
         echo "Sineware ProLinux - Plasma Mobile Nightly Image built on $(/bin/date)" >> prolinux_build_info.txt
         echo "(chroot) Exiting chroot..."
