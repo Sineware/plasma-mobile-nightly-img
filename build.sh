247add935fd3a2c38898774e934f60f40a90a1f3
Seshan Ravikumar <seshan10@me.com>
2022-11-08 17:03:03 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -89,7 +89,7 @@ EOF
     sudo rm ~/.config/pmbootstrap.cfg
 }
 
-build_image "tablet-x64uefi" "stable"
+build_image "tablet-x64uefi" "edge"
 build_image "pine64-pinephone" ""
 build_image "pine64-pinephonepro" ""
 
